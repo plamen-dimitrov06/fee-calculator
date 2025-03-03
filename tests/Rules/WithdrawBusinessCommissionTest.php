fe30fbf3f35c8352ada81276a9cc2acf58447d52
Plamen Dimitrov <plamen.dimitrov006@gmail.com>
2025-03-03 13:21:15 +0200

    add test for withdraw business commission

---
+++ tests/Rules/WithdrawBusinessCommissionTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FeeCalculator\Tests\Service;
+
+use FeeCalculator\Rules\WithdrawBusinessCommission;
+use PHPUnit\Framework\TestCase;
+use FeeCalculator\Models\Transaction;
+
+class WithdrawBusinessCommissionTest extends TestCase
+{
+    public function testDefaultValueForNonDepositOperations()
+    {
+        $transaction = $this->createStub(Transaction::class);
+        $transaction
+            ->method("isWithdraw")
+            ->willReturn(false);
+        $sut = new WithdrawBusinessCommission();
+        $this->assertEquals(0, $sut->apply($transaction));
+    }
+
+    public function testCorrectFeeCalculation()
+    {
+        $transaction = $this->createStub(Transaction::class);
+        $transaction
+            ->method("isWithdraw")
+            ->willReturn(true);
+        $transaction
+            ->method("isBusiness")
+            ->willReturn(true);
+        $transaction
+            ->method("getAmount")
+            ->willReturn(100.00);
+        $sut = new WithdrawBusinessCommission();
+        $this->assertEquals(0.5, $sut->apply($transaction));
+    }
+}
