fc14522a1d2e48289264083407470eef52a5c3f4
Plamen Dimitrov <plamen.dimitrov006@gmail.com>
2025-03-05 16:25:24 +0200

    defined all the client dependencies, remove the container instanciation (its not necessary)

--- src/ServiceProviders/ExchangeRatesProvider.php
+++ src/ServiceProviders/ExchangeRatesProvider.php
@@ -2,23 +2,31 @@
 
 namespace FeeCalculator\ServiceProviders;
 
+use FeeCalculator\Contracts\Converter;
+use FeeCalculator\HttpClients\ExchangeRatesClient;
 use GuzzleHttp\Client;
-use League\Container\Container;
 use League\Container\ServiceProvider\AbstractServiceProvider;
+use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
 
 
 class ExchangeRatesProvider extends AbstractServiceProvider
 {
     function provides(string $id): bool {
         $services = [
-            ClientInterface::class
+            ClientInterface::class,
+            Psr17Factory::class
         ];
         return in_array($id, $services);        
     }
 
     function register(): void {
-        $conatiner = new Container();
+        $conatiner = $this->getContainer();
         $conatiner->add(ClientInterface::class, Client::class);
+        $conatiner->add(RequestFactoryInterface::class, Psr17Factory::class);
+        $conatiner->add(Converter::class, ExchangeRatesClient::class)
+            ->addArgument(ClientInterface::class)
+            ->addArgument(RequestFactoryInterface::class);
     }
 }
